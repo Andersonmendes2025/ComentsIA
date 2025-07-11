942ce59f839d0b5e84ed24b5bdaf92b5d1b3e21e
kalibal7x <andreamendes042@gmail.com>
2025-07-10 23:19:58 -0300

    Atualiza models.py com RelatorioHistorico

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -19,3 +20,15 @@ class UserSettings(db.Model):
     default_greeting = db.Column(db.String(255))
     default_closing = db.Column(db.String(255))
     contact_info = db.Column(db.String(255))
+    terms_accepted = db.Column(db.Boolean, default=False)
+
+class RelatorioHistorico(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(255), nullable=False)
+    data_criacao = db.Column(db.DateTime, default=datetime.utcnow)
+    filtro_periodo = db.Column(db.String(50))
+    filtro_nota = db.Column(db.String(50))
+    filtro_respondida = db.Column(db.String(50))
+    nome_arquivo = db.Column(db.String(255))
+    caminho_arquivo = db.Column(db.String(500))
+  # Se salvar o arquivo físico no servidor
