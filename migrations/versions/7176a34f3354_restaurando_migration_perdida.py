07c8fe2c25f1ae215cc1f549f8170f3baddad5ef
kalibal7x <andreamendes042@gmail.com>
2025-08-14 10:06:52 -0300

    Restaurando migration perdida para corrigir erro no deploy do Render

---
+++ migrations/versions/7176a34f3354_restaurando_migration_perdida.py
@@ -0,0 +1,24 @@
+"""Restaurando migration perdida
+
+Revision ID: 7176a34f3354
+Revises: 5d22c8582c4b
+Create Date: 2025-08-14 10:05:57.266822
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7176a34f3354'
+down_revision = '5d22c8582c4b'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
